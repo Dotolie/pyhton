3a7c0889c9de4aa6f3deda92047fca7e739a48f1
ywkim <ywkim@solidmeca.co.kr>
2025-04-21 16:11:18 +0900

    change main.py

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ import threading
 import hashlib
 import string
 
+from io import StringIO
 
+
+COLOR_RED = "\033[31m"
+COLOR_GRE = "\033[32m"
+COLOR_END = "\033[0m"
 port = 'COM4' # 시리얼 포트
 baud = 921600 # 시리얼 보드레이트(통신속도)
 
@@ -17,8 +22,9 @@ rcv_size = 0
 rcv_datas = b''
 
 rthread = object
-readThreadRun = False
-
+readThreadRun = True
+isRun = True
+isMain = True
 
 # 파일을 읽어서 메모리에 저장하는 코드
 def read_file_to_memory(file_path):
@@ -38,24 +44,33 @@ def get_hash(data, algorithm="md5"):
 
 #쓰레드 종료용 시그널 함수
 def handler(signum, frame):
+    global readThreadRun
+    global isRun
+    global isMain
+    
     print("-----ctrl-c------")
-    readThreadRun = True
 
-    quit()
+    isMain = False
+    isRun = False
+    readThreadRun = False
+
 
 
 def readThread(ser):
+    global rcv_datas
     global rcv_size
     global rcv_datas
     global readThreadRun
 
     # 쓰레드 종료될때까지 계속 돌림
-    while not readThreadRun:
-        buf = ser.readline()
-        if len(buf) > 0 :
-            #데이터가 있있다면
-            rcv_datas = rcv_datas + buf
-            rcv_size = rcv_size + len(buf)
+    while isRun:
+        while readThreadRun:
+            buf = ser.readline()
+            if len(buf) > 0 :
+                #데이터가 있있다면
+                rcv_datas = rcv_datas + buf
+                rcv_size = rcv_size + len(buf)
+
 
 def fileWrite(datas):
     filename = "./received-"+port+".txt"
@@ -64,6 +79,18 @@ def fileWrite(datas):
     f.close()
     print("write error data")
 
+def LogWrite(datas):
+    filename = "./"+port+".log"
+    with open(filename, 'a', encoding='utf-8') as f:
+        f.write(datas+"\n")
+    f.close()
+
+
+
+def dbg(*msg):
+    io = StringIO()
+    print(*msg, file=io, end="")
+    return io.getvalue()
 
 
 def mainloop():
@@ -71,13 +98,16 @@ def mainloop():
     global rcv_datas
     global rcv_size
     global readThreadRun
+    global isMain
 
     test_count = 0
     fail_count = 0
     pass_count = 0
     
     #시리얼 열기
-    print(f"port={port}, baud={baud}")
+    wow = dbg(f"port={port}, baud={baud}")
+    print(wow)
+    LogWrite(wow)
     ser = serial.Serial(port, baud, timeout=0)
 
     # 전송 파일 열기
@@ -86,18 +116,18 @@ def mainloop():
     fil_hash = get_hash(file_content.encode('utf-8'))
 
     tt = time.strftime('%Y.%m.%d-%H:%M:%S')
-    print(f"[{tt}]                file:sz={fil_size} [{fil_hash}]")
+    wow = dbg(f"[{tt}]                file:sz={fil_size} [{fil_hash}]")
+    print(wow)
+    LogWrite(wow)
 
-    while True:
+    rthread = threading.Thread(target=readThread, args=(ser,))
+    rthread.start()
+
+    while isMain:
         test_count = test_count + 1
         
 
-        rcv_datas = b''
-        rcv_size = 0
-        readThreadRun = False
-
-        rthread = threading.Thread(target=readThread, args=(ser,))
-        rthread.start()
+        readThreadRun = True
 
         t1 = time.time()
         ts = time.strftime('%Y.%m.%d-%H:%M:%S')
@@ -111,28 +141,37 @@ def mainloop():
         t2 = time.time()
         te = time.strftime('%Y.%m.%d-%H:%M:%S')
 
-        time.sleep(3)
+        time.sleep(1)
         # 쓰레드 정지
-        readThreadRun = True
+        readThreadRun = False
+
+ 
+        if not isMain:
+            break
 
         rcv_hash = get_hash(rcv_datas)
-        
-
         if rcv_hash == fil_hash:
             pass_count = pass_count + 1
-            print(f"[{te}] [{pass_count:3d}/{test_count:3d}] {port} pass:sz={rcv_size} [{rcv_hash}] {(rcv_size*8)/(t2-t1):.0f} bps")
+            wow = dbg(f"[{te}] [{pass_count:3d}/{test_count:3d}] {port} pass:sz={rcv_size} [{rcv_hash}] {(rcv_size*8)/(t2-t1):.0f} bps")
         else:
             fail_count = fail_count + 1
-            print(f"[{te}] [{fail_count:3d}/{test_count:3d}] {port} fail:sz={rcv_size} [{rcv_hash}]")
+            wow = dbg(f"[{te}] [{fail_count:3d}/{test_count:3d}] {port} {COLOR_RED}fail{COLOR_END}:sz={rcv_size} [{rcv_hash}]")
             fileWrite(rcv_datas)
-        
-        time.sleep(3)
+
+        print(wow)
+        LogWrite(wow)
+
+        rcv_datas = b''
+        rcv_size = 0
+
+    # quit sequence
+
 
 
 
 def set_config():
     parser = argparse.ArgumentParser(
-        prog="uart-tx-sock-rx",
+        prog="repeater2test full-duplex",
         description="testing program for uart2ethernet",
     )
 
@@ -154,7 +193,7 @@ if __name__ == "__main__":
     baud = args.arg2
 
     #종료 시그널 등록
-    # signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGINT, handler)
     
 
     mainloop()
